12044e6daf9c56289c66c5de393d68bfb261859b
whats-up-nabe <tierracacerola@gmail.com>
2020-08-17 17:39:07 +0900

    add hamberger btn

--- app/assets/javascripts/modules/hamburger.js
+++ app/assets/javascripts/modules/hamburger.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log("hey");
+$(function () {
+  $(".btn-gnavi").on("click", function () {
+    let rightVal = 0;
+    if ($(this).hasClass("open")) {
+      rightVal = -300;
+      $(this).removeClass("open");
+    } else {
+      $(this).addClass("open");
+    }
+
+    $("#global-navi").stop().animate(
+      {
+        right: rightVal,
+      },
+      200
+    );
+  });
 });
