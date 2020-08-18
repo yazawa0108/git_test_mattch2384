01673ce310a8aedfe7a39c681f729333897dd05a
whats-up-nabe <tierracacerola@gmail.com>
2020-08-18 21:34:16 +0900

    done event create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,12 +10,19 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new
+    @event = Event.new(event_params)
     if @event.save
+      flash[:notice] = "案件が投稿されました。"
       redirect_to events_path
     else
       render 'event#new'
     end
   end
 
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :start_day, :end_day, :work_start_time, :work_end_time, :prefecture_id, :address, :daily_wage, :detail, :position_id, :assign_num, :place_name)
+  end
 end
