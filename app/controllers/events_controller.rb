19080ab9c9119a2e4c7718c08baed07fdb06e967
whats-up-nabe <tierracacerola@gmail.com>
2020-08-18 15:50:48 +0900

    refactoring view files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,19 @@ class EventsController < ApplicationController
   def index
   end
 
+  def new
+    @event = Event.new
+    @prefectures = Prefecture.all
+    @positions = Position.all
+  end
+
+  def create
+    @event = Event.new
+    if @event.save
+      redirect_to events_path
+    else
+      render 'event#new'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "events#index"
   devise_for :users
   resources :users, only: [:index, :show]
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new, :create]
   get "static_pages/membership_agreement", to: "static_pages#membership_agreement"
 end
