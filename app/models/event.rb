5619471372c8a0fc32ff70b64500f02d9cde7c63
whats-up-nabe <tierracacerola@gmail.com>
2020-08-19 11:39:13 +0900

    update error messages

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
   belongs_to :prefecture
   belongs_to :position
   belongs_to :event_status
+
+  validates :name, presence: true
+  validates :start_day, presence: true
+  validates :prefecture_id, presence: true
+  validates :position_id, presence: true
+  validates :assign_num, presence: true
 end
