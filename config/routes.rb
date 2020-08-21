a562a0b95aadf0b0cc92787b7225c831a61a1727
whats-up-nabe <tierracacerola@gmail.com>
2020-08-21 11:21:40 +0900

    nest applicant to event

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "events#index"
   devise_for :users
   resources :users, only: [:index, :show]
-  resources :events, only: [:index, :show, :new, :create]
-  resources :applicants, only: [:create, :destroy]
+  resources :events, only: [:index, :show, :new, :create] do
+    resources :applicants, only: [:create, :destroy]
+  end
   get "static_pages/membership_agreement", to: "static_pages#membership_agreement"
 end
