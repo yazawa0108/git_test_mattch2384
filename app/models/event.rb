b14527bf8de68c64d433298ad1891aa815b8583f
whats-up-nabe <tierracacerola@gmail.com>
2020-08-21 17:33:46 +0900

    done

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,16 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :start_day, presence: true
+  validate :end_day, :check_end
   validates :prefecture_id, presence: true
   validates :position_id, presence: true
   validates :assign_num, presence: true
+
+  private
+
+  def check_end
+    if end_day.present? && start_day > end_day
+      errors.add(:end_day, "を開始日以降に設定してください。")
+    end
+  end
 end
