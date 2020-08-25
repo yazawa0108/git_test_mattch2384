ae233dc66a846fa18cb515893f6f790b52644479
Nickthetech <68375340+Nickthetech@users.noreply.github.com>
2020-08-25 20:29:52 +0900

    showアクションの完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+#    @events = Event.where(owner_id: current_user.id)
+#    @array = []
+#    @events.each do |event|
+#      event.applicants.each do |applicant|
+#        @array << applicant.applicant_user_id
+#      end
+#    end
+
+    @applicants = Applicant.where(applicant_user_id: @user.id)
+    binding.pry
+    @owners = []
+      @applicants.each do |applicant|
+        @owners << Event.where(id: applicant.event_id).owner_id
+      end
+    
+
+  end
 
   private
 
