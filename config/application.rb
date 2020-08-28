b80f107c3d2ccf0cab707833f9aebfed06b4bb4d
Nickthetech <68375340+Nickthetech@users.noreply.github.com>
2020-08-28 15:18:16 +0900

    first commit

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module Match2384
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
   end
 end
